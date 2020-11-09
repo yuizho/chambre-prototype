73caa7089b74332a3788c0a325e2e179a90f751b
yuizho <yuizho3@gmail.com>
2020-11-10 00:06:50 +0900

    define User class

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import UserList from './components/UserList';
 const App: FC = () => (
   <div className="App">
     <main>
-      <UserList users={['a', 'b', 'c']} />
+      <UserList
+        users={[
+          { id: '0', name: 'admin', role: 1 },
+          { id: '1', name: 'ichiro', role: 0 },
+        ]}
+      />
     </main>
   </div>
 );
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,29 @@
 import React, { FC } from 'react';
 
+type Role = 0 | 1;
+
+class User {
+  id: string;
+  name: string;
+  role: Role;
+  constructor(id: string, name: string, role: Role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
+}
+
 interface UserListProp {
-  users: Array<string>;
+  users: Array<User>;
 }
 
 const UserList: FC<UserListProp> = (props: UserListProp) => {
   const users = props.users;
-  const listItems = users.map((user) => <li key={user}>{user}</li>);
+  const listItems = users.map((user) => (
+    <li key={user.id}>
+      {user.name}@{user.role}
+    </li>
+  ));
   return <ul>{listItems}</ul>;
 };
 
